0a82a921a66ad9fdc2f911851fe64cb95286003b
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2025-02-28 00:30:50 +0100

    test cicd

--- app/src/utils/axios.ts
+++ app/src/utils/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api";
+const API_URL =
+  process.env.REACT_APP_API_URL || "https://cfainsta.alkwelo.app/api";
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
