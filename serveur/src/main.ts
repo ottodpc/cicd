e7908a6601c16cde44a5d3a6be045b0e93aa3bf9
OTTO Dieu-Puissant Cyprien <cyprienotto@outlook.fr>
2025-02-28 01:01:31 +0100

    ohllala server test s

--- serveur/src/main.ts
+++ serveur/src/main.ts
@@ -10,7 +10,7 @@ import { LoggerAdapter } from './logger/logger.adapter';
 
 import { ConfigService } from '@nestjs/config';
 
-// ohllala server tests
+// ohllala server test s
 export const getApp = async () => {
   const app = await NestFactory.create(AppModule);
   const httpAdapterHost = app.get(HttpAdapterHost);
